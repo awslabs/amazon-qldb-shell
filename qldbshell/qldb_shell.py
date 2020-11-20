feab7acff4df018c2b2028ea423f25db8093c4e0
Byron Lin <byronl@bitquilltech.com>
2020-11-20 14:41:03 -0800

    Parse user input as string literal

--- qldbshell/qldb_shell.py
+++ qldbshell/qldb_shell.py
@@ -10,7 +10,9 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
 # express or implied. See the License for the specific language governing 
 # permissions and limitations under the License.
+from ast import literal_eval
 import logging
+from shlex import quote
 from textwrap import dedent
 
 import prompt_toolkit
@@ -109,6 +111,7 @@ class QldbShell:
             try:
                 text = shell_session.prompt(self.prompt)
                 text = text.strip()
+                text = literal_eval(quote(text))
                 if text:
                     self.onecmd(text)
             except KeyboardInterrupt:
