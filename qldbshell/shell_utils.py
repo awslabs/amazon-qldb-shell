a2292c748e5df313ee4b6f561ad7d8977b89ba76
Byron Lin <byronl@bitquilltech.com>
2020-11-23 12:10:56 -0800

    Remove unused import and reverted formatting change

--- qldbshell/shell_utils.py
+++ qldbshell/shell_utils.py
@@ -4,7 +4,8 @@ from amazon.ion.simpleion import dumps
 
 
 def print_result(cursor: BufferedCursor):
-    results = list(map(lambda x: dumps(x, binary=False, indent=' ', omit_version_marker=True), cursor))
+    results = list(map(lambda x: dumps(x, binary=False,
+                                       indent=' ', omit_version_marker=True), cursor))
     logging.info("\n" + str(',\n').join(results))
 
 
