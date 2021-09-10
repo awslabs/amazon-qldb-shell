e57f7fbf8d107f1ea150b2935c9c3f530ba6abe3
Yilin Guo <guyilin@amazon.com>
2021-09-10 11:24:06 -0700

    Remove markdown links

--- src/settings/command_line.rs
+++ src/settings/command_line.rs
@@ -13,8 +13,8 @@ use crate::error::{usage_error, ShellError};
 /// see the shell topic in the QLDB Developer Guide: https://docs.aws.amazon.com/qldb/latest/developerguide/data-shell.html
 #[structopt(name = "qldb")]
 pub struct Opt {
-    /// The AWS Region code of the QLDB ledger to connect to. For example: `us-east-1`.
-    /// By default, the shell will pick a default region as described in the shell guide
+    /// The AWS Region code of the QLDB ledger to connect to. For example: us-east-1.
+    /// By default, the shell will pick a default region as described in the shell guide.
     #[structopt(short, long = "--region")]
     pub region: Option<String>,
 
@@ -29,14 +29,13 @@ pub struct Opt {
     #[structopt(short, long, parse(from_os_str))]
     pub config: Option<PathBuf>,
 
-    /// The `qldb-session` API endpoint to connect to.
-    /// For a complete list of available QLDB Regions and endpoints,
-    /// see [Amazon QLDB endpoints and quotas](https://docs.aws.amazon.com/general/latest/gr/qldb.html).
+    /// The qldb-session API endpoint to connect to.
+    /// For a complete list of available QLDB Regions and endpoints, see the shell guide.
     #[structopt(short = "-s", long = "--qldb-session-endpoint", parse(try_from_str = Url::try_from))]
     pub qldb_session_endpoint: Option<Url>,
 
     /// The location of your AWS credentials profile to use for authentication.
-    /// By default, the shell will pick a default profile as described in the shell guide
+    /// By default, the shell will pick a default profile as described in the shell guide.
     #[structopt(short, long = "--profile")]
     pub profile: Option<String>,
 
@@ -46,7 +45,7 @@ pub struct Opt {
     #[structopt(short, long = "--verbose", parse(from_occurrences))]
     pub verbose: u8,
 
-    /// The output format of your query results. By default, the format is `ion`.
+    /// The output format of your query results. By default, the format is ion.
     #[structopt(short, long = "--format")]
     pub format: Option<FormatMode>,
 }
